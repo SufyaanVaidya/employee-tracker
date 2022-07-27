715c03291b664c33c13c5064be532f6f761a117c
Sufyaan Vaidya <vaidyasufyaan@gmail.com>
2022-07-27 14:46:48 -0400

    created employee table

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,6 @@ USE employee_db;
 CREATE TABLE department (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
@@ -13,6 +12,16 @@ CREATE TABLE role (
     title VARCHAR(30) NOT NULL, 
     salary DECIMAL NOT NULL,
     department_id INTEGER,
-    PRIMARY KEY (id),
     FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 )
+
+CREATE TABLE employee (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
+
+);
