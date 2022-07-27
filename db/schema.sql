35eb1f7556f5d6d3393ab820df95d12f65dfb6bf
Sufyaan Vaidya <vaidyasufyaan@gmail.com>
2022-07-27 14:37:40 -0400

    created role table

--- db/schema.sql
+++ db/schema.sql
@@ -4,6 +4,15 @@ USE employee_db;
 
 CREATE TABLE department (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
+CREATE TABLE role (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL, 
+    salary DECIMAL NOT NULL,
+    department_id INTEGER,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+)
