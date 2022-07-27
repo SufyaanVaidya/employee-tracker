0c29cf379b876c41f1b918bbf933c2e303e3f10a
Sufyaan Vaidya <vaidyasufyaan@gmail.com>
2022-07-27 16:19:54 -0400

    created connection to sql

--- server.js
+++ server.js
@@ -3,3 +3,15 @@ const mysql = require('mysql2');
 const consoleTable = require('console.table');
 require('dotenv').config()
 
+const sqlConnection = mysql.createConnection({
+    host: 'localhost',
+    user: process.env.User,
+    password: process.env.Password,
+    database: process.env.Database
+});
+
+sqlConnection.connect(err => {
+    if (err)
+    throw (err);
+    console.log('You Have Been Connected!')
+});
