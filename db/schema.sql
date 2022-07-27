fbfee12fd23252a2cb7399b96a8f5e8d38163d28
Sufyaan Vaidya <vaidyasufyaan@gmail.com>
2022-07-27 14:24:12 -0400

    created department table

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,9 @@
 DROP DATABASE IF EXIST employee_db;
 CREATE DATABASE employee_db;
 USE employee_db;
+
+CREATE TABLE department (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL
+);
+
