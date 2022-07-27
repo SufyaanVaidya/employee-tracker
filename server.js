b282418cd0c1196117a8627af473d09397fcd1a5
Sufyaan Vaidya <vaidyasufyaan@gmail.com>
2022-07-27 16:33:02 -0400

    added initial prompt

--- server.js
+++ server.js
@@ -14,4 +14,29 @@ sqlConnection.connect(err => {
     if (err)
     throw (err);
     console.log('You Have Been Connected!')
+    initialPrompt();
 });
+
+const initialPrompt = () => {
+    inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'choices',
+            message: 'Please select a process to begin',
+            choices: [
+                'View All Employees',
+                'Add Employee',
+                'Update Employee Role',
+                'View All Roles',
+                'Add A Role',
+                'View All Departments',
+                'Add Department',
+            ]
+        }
+    ])
+    .then((answers) => {
+        const { choices } = answers;
+        
+    })
+}
