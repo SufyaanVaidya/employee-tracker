92273d1c2207cad3cc17b59aea96a5b2178090cd
Sufyaan Vaidya <vaidyasufyaan@gmail.com>
2022-07-28 18:13:53 -0400

    added variable for how the data will populate

--- server.js
+++ server.js
@@ -62,7 +62,18 @@ const initialPrompt = () => {
 }
 
 function allEmployees() {
-
+const sqlQuery = `
+SELECT employee.id,
+employee.first_name,
+employee.last_name,
+role.title,
+department.name AS department,
+role.salary,
+concat(manager.first_name, ' ', manager.last_name) AS manager FROM employee
+LEFT JOIN role ON employee.role_id = role.id
+LEFT JOIN department ON role.department_id = department.id
+LEFT JOIN employee manager ON employee.manager_id = manager.id
+`;
 };
 
 function addEmployees() {
